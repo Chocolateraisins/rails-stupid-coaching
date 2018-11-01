98a8e22e76480767b91bcece3cb8a3e32e23aa82
Stephen Schuez <stephen.schuez@gmail.com>
2018-11-01 14:55:08 +0800

    updated ask and answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+  # get 'questions/ask'
+  # get 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+  # verb 'path', to: 'controller#action', as: :route_name
